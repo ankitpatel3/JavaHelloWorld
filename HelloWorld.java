8ef1cda48a9107468c1027d814db6cf2f35a7307
root <root@5687ab9783f2.(none)>
2019-02-15 17:53:53 +0000

    Commiting First of many files

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello everyone in the world");
+		System.out.println("This is the first step in Jenkins pipeline");		
+	}	
+}
