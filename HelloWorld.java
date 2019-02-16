c1de443235ae83e52351e84f99353248f4a95649
ankitpatel3 <47609132+ankitpatel3@users.noreply.github.com>
2019-02-15 23:26:37 -0500

    Adding GitHub push message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello everyone in the world");
-		System.out.println("This is the first step in Jenkins pipeline");		
+		System.out.println("This is the first step in Jenkins pipeline");
+		System.out.println("Wow, this time from the GitHub Push event");
 	}	
 }
