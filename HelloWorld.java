6f5f951db42e280b48a192f042ca6490fa0ffcae
ankitpatel3 <47609132+ankitpatel3@users.noreply.github.com>
2019-02-15 23:29:03 -0500

    After webhook is set

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello everyone in the world");
 		System.out.println("This is the first step in Jenkins pipeline");
-		System.out.println("Wow, this time from the GitHub Push event");
+		System.out.println("Wow, this is triggered by GitHub push event");
+		
 	}	
 }
